fe9eea27dbf8bf462cefe1d4f89c7b9cb42421b9
Fogotcheck <fogotcheck@live.com>
2025-05-24 11:21:14 +0300

    build: inittial

---
+++ AppMain/AppMain.cpp
@@ -0,0 +1,32 @@
+#include <stdexcept>
+
+#include "main.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "event_groups.h"
+#include "cmsis_os2.h"
+
+void MainThr(__attribute__((unused)) void *arg);
+
+int main(void)
+{
+	system_init();
+	BaseType_t ret = xTaskCreate(MainThr, "MainTask", 500, NULL, 1, NULL);
+	if (ret != pdPASS) {
+		Error_Handler();
+	}
+	vTaskStartScheduler();
+	Error_Handler();
+}
+
+void MainThr(__attribute__((unused)) void *arg)
+{
+	if (osKernelInitialize()) {
+		Error_Handler();
+	}
+
+	while (1) {
+		HAL_GPIO_TogglePin(LD1_GPIO_Port, LD1_Pin);
+		vTaskDelay(1000);
+	}
+}
---
+++ CHANGELOG.md
@@ -0,0 +1,16 @@
+# **[${PROJECT_NAME}-${PROJECT_VERSION}](https://github.com/Fogotcheck/)**
+
+## *Инфо*
+```
+Project         ::  ${PROJECT_NAME}
+Data            ::  ${TODAY}
+Version         ::  ${PROJECT_VERSION}
+Commit          ::  ${GIT_REV_PARSE}
+Compiler        ::  ${CMAKE_CXX_COMPILER_ID}:${CXX_COMPILER_NAME}-${CMAKE_CXX_COMPILER_VERSION}
+                ::  ${CMAKE_C_COMPILER_ID}:${C_COMPILER_NAME}-${CMAKE_C_COMPILER_VERSION}
+Build type      ::  ${CMAKE_BUILD_TYPE}
+```
+
+*Добавлено*
+- 2025_24_05
+    1. базовая структура проекта
